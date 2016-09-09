58b6ff977b943c28307f8a9e841073b315f69772
Makoto Inoue <inouemak@googlemail.com>
2016-09-10 00:27:11 +0100

    Add basic style

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -7,7 +7,6 @@ import FlatButton from 'material-ui/FlatButton';
 import Avatar from 'material-ui/Avatar';
 import AppBar from 'material-ui/AppBar';
 import {List, ListItem} from 'material-ui/List';
-import EventEmitter from 'event-emitter';
 import Paper from 'material-ui/Paper';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -29,35 +28,43 @@ function setup(){
   });
 }
 
+const App = (props) => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <div>
+      <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'sans-serif'}} style={{backgroundColor:"#46ae32"}}
+        title={
+          <span>TITLE<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - Some one liner -</span></span>
+        }
+        iconElementLeft={<Avatar src="https://15254b2dcaab7f5478ab-24461f391e20b7336331d5789078af53.ssl.cf1.rackcdn.com/ethereum.vanillaforums.com/favicon_85d47ba50743e3c3.ico" size={50} backgroundColor="white" />}
+        iconElementRight={
+          <span>
+            <FlatButton style={{color:'white'}} label="About" />
+          </span>
+        }
+      />
+      <div className='container'>
+        <Paper zDepth={1} style={{height:'500px'}}>
+          <List>
+            <ListItem disabled={true}
+              primaryText={(<p>Content</p>)}
+            />
+          </List>
+        </Paper>
+        <Paper zDepth={1}>
+          <List>
+            <ListItem disabled={true}
+              primaryText={(<p>Some footer</p>)}
+            />
+          </List>
+        </Paper>
+      </div>
+    </div>
+  </MuiThemeProvider>
+)
+
 window.onload = function() {
-  console.log('loaded')
   setup().then(({provider, web3}) => {
     web3.setProvider(provider);
-    window.web3 = web3
-    const eventEmitter = EventEmitter()
-
-    const App = (props) => (
-      <div>
-        <MuiThemeProvider muiTheme={getMuiTheme()}>
-          <div>
-            <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'Lobster'}} style={{backgroundColor:"#607D8B"}}
-              title={
-                <span>TITLE<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - Some one liner -</span></span>
-              }
-              iconElementLeft={<Avatar src="" size={50} backgroundColor="rgb(96, 125, 139)" />}
-              iconElementRight={
-                <span>
-                  <FlatButton style={{color:'white'}} label="About" />
-                </span>
-              }
-            />
-            <div className='container'>
-            </div>
-          </div>
-        </MuiThemeProvider>
-      </div>
-    );
-
     injectTapEventPlugin();
     ReactDOM.render(
       <App/>,
