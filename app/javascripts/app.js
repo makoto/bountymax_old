752f585871452b4de3874ff322004413f16cd1cf
Makoto Inoue <inouemak@googlemail.com>
2016-09-09 23:23:13 +0100

    Move app to app_org.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,55 +1,67 @@
-var accounts;
-var account;
+import {} from "../stylesheets/app.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import Avatar from 'material-ui/Avatar';
+import AppBar from 'material-ui/AppBar';
+import {List, ListItem} from 'material-ui/List';
+import EventEmitter from 'event-emitter';
+import Paper from 'material-ui/Paper';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
-};
-
-function refreshBalance() {
-  var meta = MetaCoin.deployed();
-
-  meta.getBalance.call(account, {from: account}).then(function(value) {
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting balance; see log.");
+function setup(){
+  return new Promise(function(resolve,reject){
+    let provider;
+    let url = "http://localhost:8545";
+    // mist loading proposal https://gist.github.com/frozeman/fbc7465d0b0e6c1c4c23
+    if(typeof web3 !== 'undefined'){
+      provider = web3.currentProvider;
+      web3 = new Web3;
+      resolve({web3, provider})
+    }else{
+      // connect to localhost
+      provider = new Web3.providers.HttpProvider(url);
+      let web3 = new Web3;
+      resolve({web3, provider})
+    }
   });
-};
-
-function sendCoin() {
-  var meta = MetaCoin.deployed();
-
-  var amount = parseInt(document.getElementById("amount").value);
-  var receiver = document.getElementById("receiver").value;
-
-  setStatus("Initiating transaction... (please wait)");
-
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-};
+}
 
 window.onload = function() {
-  web3.eth.getAccounts(function(err, accs) {
-    if (err != null) {
-      alert("There was an error fetching your accounts.");
-      return;
-    }
+  console.log('loaded')
+  setup().then(({provider, web3}) => {
+    web3.setProvider(provider);
+    window.web3 = web3
+    const eventEmitter = EventEmitter()
 
-    if (accs.length == 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
-    }
+    const App = (props) => (
+      <div>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <div>
+            <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'Lobster'}} style={{backgroundColor:"#607D8B"}}
+              title={
+                <span>TITLE<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - Some one liner -</span></span>
+              }
+              iconElementLeft={<Avatar src="" size={50} backgroundColor="rgb(96, 125, 139)" />}
+              iconElementRight={
+                <span>
+                  <FlatButton style={{color:'white'}} label="About" />
+                </span>
+              }
+            />
+            <div className='container'>
+            </div>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
 
-    accounts = accs;
-    account = accounts[0];
-
-    refreshBalance();
-  });
+    injectTapEventPlugin();
+    ReactDOM.render(
+      <App/>,
+      document.getElementById('app')
+    );
+  })
 }
