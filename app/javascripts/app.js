65d5c1550a378c0d30d3df064383e8a781f8733f
Makoto Inoue <inouemak@googlemail.com>
2016-09-10 08:51:23 +0100

    Move app to component

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,14 +1,8 @@
 import {} from "../stylesheets/app.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import FlatButton from 'material-ui/FlatButton';
-import Avatar from 'material-ui/Avatar';
-import AppBar from 'material-ui/AppBar';
-import {List, ListItem} from 'material-ui/List';
-import Paper from 'material-ui/Paper';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import App from './components/app';
 
 function setup(){
   return new Promise(function(resolve,reject){
@@ -27,40 +21,6 @@ function setup(){
     }
   });
 }
-
-const App = (props) => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <div>
-      <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'sans-serif'}} style={{backgroundColor:"#46ae32"}}
-        title={
-          <span>TITLE<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - Some one liner -</span></span>
-        }
-        iconElementLeft={<Avatar src="https://15254b2dcaab7f5478ab-24461f391e20b7336331d5789078af53.ssl.cf1.rackcdn.com/ethereum.vanillaforums.com/favicon_85d47ba50743e3c3.ico" size={50} backgroundColor="white" />}
-        iconElementRight={
-          <span>
-            <FlatButton style={{color:'white'}} label="About" />
-          </span>
-        }
-      />
-      <div className='container'>
-        <Paper zDepth={1} style={{height:'500px'}}>
-          <List>
-            <ListItem disabled={true}
-              primaryText={(<p>Content</p>)}
-            />
-          </List>
-        </Paper>
-        <Paper zDepth={1}>
-          <List>
-            <ListItem disabled={true}
-              primaryText={(<p>Some footer</p>)}
-            />
-          </List>
-        </Paper>
-      </div>
-    </div>
-  </MuiThemeProvider>
-)
 
 window.onload = function() {
   setup().then(({provider, web3}) => {
